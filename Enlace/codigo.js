dd2e92932c43c4ba682d6bb0accfb5ecede43445
flavioOchoa1 <flavio.ochoavi@ug.edu.ec>
2025-03-14 13:15:11 -0500

    javascript - 1 enlaces

---
+++ Enlace/codigo.js
@@ -0,0 +1,12 @@
+string = "mundo";
+let numero;
+numero = 5;
+
+booleano = true;
+
+
+const constante = "constante";
+console.log("Hola " + string);
+console.log("Hola " + numero); 
+alert("Hola " + string);
+//alert("hola mundo - minuto 43");
