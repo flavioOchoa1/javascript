48620dec9e5c768c42ea2960a10e102938bbbdcd
flavioOchoa1 <flavio.ochoavi@ug.edu.ec>
2025-03-19 18:03:36 -0500

    actualización 2

--- Enlace/codigo.js
+++ Enlace/codigo.js
@@ -1,12 +1,27 @@
 string = "mundo";
-let numero;
-numero = 5;
-
+let numero = 5;
+let numero1 = 5;
+numero += 5; //operador de asignación
 booleano = true;
-
-
+console.log(numero);
 const constante = "constante";
 console.log("Hola " + string);
 console.log("Hola " + numero); 
 alert("Hola " + string);
 //alert("hola mundo - minuto 43");
+//prompt("hola mundo - minuto 43");
+//confirm("hola mundo - minuto 43");
+//let nombre = prompt("Ingresa tu nombre");
+//console.log("Hola " + nombre);  //minuto 45
+//let edad = prompt("Ingresa tu edad");   //minuto 46
+//console.log("Tienes " + edad + " años");    //minuto 46
+resultado = numero1&numero;
+alert(resultado);
+console.log(resultado);
+numero--; //postdecremento
+console.log(numero);
+document.write(numero);  //minuto 47
+
+nombre = "Arturo";
+frase = `Hola ${nombre} como estas?`; 
+document.write(frase);  //minuto 48
